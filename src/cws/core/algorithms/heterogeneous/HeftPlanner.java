d56dfd44b9923eb2e5b5fb051b2a7e4403c0a75f
David Shepherd <davidshepherd7@gmail.com>
2014-11-28 10:59:06 +0000

    Add Heft work from other branch
    Had to rewrite a lot of stuff, so not a merge

---
+++ src/cws/core/algorithms/heterogeneous/HeftPlanner.java
@@ -0,0 +1,124 @@
+package cws.core.algorithms.heterogeneous;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map; 
+
+import org.cloudbus.cloudsim.Log;
+
+import cws.core.VM;
+import cws.core.core.VMType;
+
+import cws.core.dag.DAG;
+import cws.core.dag.Task;
+import cws.core.dag.algorithms.CriticalPath;
+import cws.core.dag.algorithms.TopologicalOrder;
+
+import cws.core.engine.Environment;
+import cws.core.cloudsim.CloudSimWrapper;
+
+import cws.core.algorithms.Plan;
+import cws.core.algorithms.Plan.Resource;
+import cws.core.algorithms.Plan.Solution;
+import cws.core.algorithms.Plan.Slot;
+import cws.core.algorithms.Plan.NoFeasiblePlan;
+
+
+
+/**
+ * TODO: implement storage aware version (i.e. non-zero data transfer time).
+ *      
+ */
+public class HeftPlanner implements Planner {
+
+    @Override
+    public Plan planDAG(DAG dag, List<VMType> availableVMTypes) throws NoFeasiblePlan {
+        return new Plan();
+    }
+
+    // Get computation time for a job on a vm.
+    public static double compCost(Task job, VMType vmtype) {
+        return job.getSize() / vmtype.getMips();
+    }
+
+    // Get average (over all VMs) of the time taken for this task.
+    public static double meanComputationTime(Task task, Map<VMType, Integer> nvms) {
+        double total = 0.0;
+        int count = 0;
+        for(Map.Entry<VMType, Integer> entry : nvms.entrySet()) {
+            total += compCost(task, entry.getKey()) * entry.getValue();
+            count += entry.getValue();
+        }
+        return total / count;
+    }
+
+
+    /**
+     * Get average communication cost of the variables transferred between
+     * jobs t1 and t2 between all pairs of VMs. NOT IMPLEMENTED
+     */
+    public static double cBar(Task t1, Task t2) {
+        throw new UnsupportedOperationException("communication times not implemented."); 
+    }
+
+
+    /**
+     * Compute rank of a job, based on computation time, rank of successors
+     * and transfer time to successors.
+     */
+    public static double rankU(Task task, Map<VMType, Integer> vmNumbers) {
+
+        double max_child_rank = 0.0;
+
+        // If we have children then recurse and calculate their max rank
+        List<Task> children = task.getChildren();
+        if(children.size() > 0) {
+            for(Task child : children) {
+                double crank = rankU(child, vmNumbers); // + cBar(child, task);
+                max_child_rank = Math.max(crank, max_child_rank);
+            }
+        }
+
+        return max_child_rank + meanComputationTime(task, vmNumbers);
+    }
+
+
+    /**
+     * Get list of tasks in a DAG ordered by rankU().
+     */
+    public static List<Task> rankedTasks(DAG dag, Map<VMType, Integer> vmNumbers) {
+
+        // Get tasks (this code is crap but I don't want to change the core
+        // library too much so we're stuck with it...)
+        List<Task> sorted = new ArrayList<Task>();
+        for(String id : dag.getTasks()) {
+            sorted.add(dag.getTaskById(id));
+        }
+
+        // Get all ranks and store in map
+        final Map<Task, Double> ranks = new HashMap<Task, Double>();
+        for(Task t : sorted) {
+            ranks.put(t, rankU(t, vmNumbers));
+        } 
+        
+        // Construct function to sort by rank
+        Comparator<Task> compare = new Comparator<Task>() {
+            @Override
+            public int compare(Task t1, Task t2) {
+                return Double.compare(ranks.get(t1), ranks.get(t2));
+            }
+        };
+
+        // Sort the list by rank, actually we want it reversed (highest
+        // rank first).
+        Collections.sort(sorted, Collections.reverseOrder(compare));
+
+        return sorted;
+    }
+
+
+}
---
+++ test/cws/core/algorithms/heterogeneous/HeftPlannerTest.java
@@ -0,0 +1,156 @@
+package cws.core.algorithms.heterogeneous;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+
+import cws.core.VM;
+import cws.core.core.VMType;
+import cws.core.core.VMTypeBuilder;
+import cws.core.VMFactory;
+
+import cws.core.cloudsim.CloudSimWrapper;
+import cws.core.engine.Environment;
+
+
+import cws.core.dag.Task;
+import cws.core.dag.DAG;
+
+
+//??ds pull out some helper functions? eg. dag makers
+
+
+// Based on https://github.com/mrocklin/heft/blob/master/heft/tests/test_core.py
+public class HeftPlannerTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    public VMType makeVM(double mips) {
+        return VMTypeBuilder.newBuilder().mips(mips).
+                cores(1).price(1.0).build();
+    }
+
+    public Map<VMType, Integer> makeUniformVMS() {
+
+        // 3 identical VMs
+        Map<VMType, Integer> agents = new HashMap<>();
+        agents.put(makeVM(1.0), 3);
+
+        return agents;
+    }
+
+    public Map<VMType, Integer> makeNonUniformVMS() {
+
+        // One each of three different VMs
+        Map<VMType, Integer> agents = new HashMap<>();
+        agents.put(makeVM(0.5), 1);
+        agents.put(makeVM(0.25), 1);
+        agents.put(makeVM(1.0), 1);
+
+        return agents;
+    }
+
+
+    @Test
+    public void testVMEquality() {
+        VMType a = makeVM(2.0);
+        VMType b = makeVM(2.0);
+        VMType c = makeVM(3.0);
+
+        Assert.assertTrue(a.equals(a));
+        Assert.assertFalse(a.equals(b));
+        Assert.assertFalse(a.equals(c));
+    }
+
+    @Test
+    public void testWBar() {
+        Task myTask = new Task("a", "", 1.0);
+
+        assertThat(1.0,
+                is(HeftPlanner.meanComputationTime(myTask, makeUniformVMS())));
+
+        assertThat((2.0 + 4 + 1)/3, 
+                is(HeftPlanner.meanComputationTime(myTask, makeNonUniformVMS())));
+
+    }
+
+
+    public DAG makeTasks() {
+        // dag = {a: (c,),
+        //        b: (d,),
+        //        c: (e,),
+        //        d: (e,),
+        //        e: (f, g)}
+
+        DAG dag = new DAG();
+        dag.addTask(new Task("a", "", 1.0));
+        dag.addTask(new Task("b", "", 1.0));
+        dag.addTask(new Task("c", "", 1.0));
+        dag.addTask(new Task("d", "", 1.0));
+        dag.addTask(new Task("e", "", 1.0));
+        dag.addTask(new Task("f", "", 1.0));
+        dag.addTask(new Task("g", "", 1.5)); // break the f vs g symmetry
+
+        dag.addEdge("a", "c");
+        dag.addEdge("b", "d");
+        dag.addEdge("c", "e");
+        dag.addEdge("d", "e");
+        dag.addEdge("e", "f");
+        dag.addEdge("e", "g");
+
+        return dag;
+    }
+
+    @Test
+    public void testRankU() {
+
+        DAG tasks = makeTasks();
+        Map<VMType, Integer> vms = makeUniformVMS();
+
+        // Check that the final tasks only depend on themselves
+        assertThat(HeftPlanner.rankU(tasks.getTaskById("f"), vms),
+                is(HeftPlanner.meanComputationTime(tasks.getTaskById("f"), vms)));
+                
+
+        // Check for one task deeper into the dag (assuming that f and g
+        // cost the same).
+        double expectedRank = HeftPlanner.meanComputationTime(tasks.getTaskById("e"), vms)
+                // + HeftPlanner.cBar(tasks.getTaskById("e"), tasks.getTaskById("g"))
+                + HeftPlanner.rankU(tasks.getTaskById("g"), vms);
+
+        assertThat(HeftPlanner.rankU(tasks.getTaskById("e"), vms), is(expectedRank)); 
+    }
+
+    @Test
+    public void testRankedTasks() {
+        DAG dag = makeTasks();
+
+        // The correct result
+        List<Task> expected = new ArrayList<Task>();
+        expected.add(dag.getTaskById("b"));
+        expected.add(dag.getTaskById("a"));
+        expected.add(dag.getTaskById("d"));
+        expected.add(dag.getTaskById("c"));
+        expected.add(dag.getTaskById("e"));
+        expected.add(dag.getTaskById("g"));
+        expected.add(dag.getTaskById("f"));
+
+        List<Task> actual = HeftPlanner.rankedTasks(dag, makeUniformVMS());
+        assertThat(actual, is(expected));
+    }
+
+
+
+}
